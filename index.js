f897df3bd7ef1a1c9026df19750bdd6773b3fe98
Patryk <jpatryk0494@gmail.com>
2020-11-24 20:40:08 +0100

    application working fine

--- index.js
+++ index.js
@@ -51,7 +51,43 @@ function onOutAddBtnClicked() {
 
     updateMainContentnListUI();
     sum();
-}       
+}  
+
+function onDeleteBtnClicked(event) {
+    inOutData = inOutData.filter(function(elem){
+        return elem.id !== Number(event.target.id.split('-')[1]);
+    });
+    localStorage.setItem("inOutArr", JSON.stringify(inOutData));
+    
+    updateMainContentnListUI();
+    sum();
+}
+
+function onEditBtnClicked(event){
+    indexElem = inOutData.findIndex(function(elem){
+        return elem.id === Number(event.target.id.split('-')[1]);
+    });
+    let container = document.getElementById(`container-${event.target.id.split('-')[1]}`);
+    // container.classList = ("z-index");
+    // shadow = document.createElement("div");
+    // shadow.classList = ("shadow");
+    // document.body.appendChild(shadow);
+    container.innerHTML =`
+            <input class="edited-name" id="edited-name" type="text" value="${inOutData[indexElem].name}" placeholder="nazwa przychodu">
+            <input class="edited-val" id="edited-val" type="number" value="${inOutData[indexElem].value}" placeholder="kwota przychodu">
+            <button class="addBtn" id="edited-addBtn">Dodaj</button>
+        `; 
+    const editedAddBtn = document.getElementById("edited-addBtn")
+    editedAddBtn.addEventListener("click", function() {
+        const editedName = document.getElementById("edited-name");
+        const editedVal = document.getElementById("edited-val");
+        inOutData[indexElem].name = editedName.value;
+        inOutData[indexElem].value = editedVal.value;
+        updateMainContentnListUI()
+        sum();
+        // shadow.remove();
+    });   
+}
     
 function updateMainContentnListUI () {
     const mainContentIncomeList = document.getElementById("mainContent-income-list");
@@ -63,17 +99,20 @@ function updateMainContentnListUI () {
     inOutData.forEach(function(inOutDataElem) {
         const container = document.createElement("div");
         container.classList = "list-element"
+        container.id = `container-${inOutDataElem.id}`;
 
         const paragraph = document.createElement("p");
         paragraph.innerText = `${inOutDataElem.name}:  ${inOutDataElem.value} zł`
 
         const editBtn = document.createElement("button");
         editBtn.innerText = "Edytuj"
-        editBtn.id = `edit_${inOutDataElem.id}`
+        editBtn.id = `edit-${inOutDataElem.id}`;
+        editBtn.addEventListener("click", onEditBtnClicked);
 
         const deleteBtn = document.createElement("button");
         deleteBtn.innerText = "Usuń"
-        deleteBtn.id = `delete_${inOutDataElem.id}`
+        deleteBtn.id = `edit-${inOutDataElem.id}`;
+        deleteBtn.addEventListener("click", onDeleteBtnClicked)
 
         container.appendChild(paragraph);
         container.appendChild(editBtn);
